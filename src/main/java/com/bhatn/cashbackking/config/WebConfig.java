5864c9e661ce8cd53fab1200f9e9a4911f1a0cdf
Prachi <bhatnagar.prachi845@gmail.com>
2026-03-26 12:11:32 -0400

    Added CORS

---
+++ src/main/java/com/bhatn/cashbackking/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.bhatn.cashbackking.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://feature-initialcommit.dwp81oqt95zeu.amplifyapp.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
