b2cc23999c379f7ac769e238094ccab5e8925401
Prachi <bhatnagar.prachi845@gmail.com>
2026-03-27 13:51:11 -0400

    Added CORS

--- src/main/java/com/bhatn/cashbackking/StreamLambdaHandler.java
+++ src/main/java/com/bhatn/cashbackking/StreamLambdaHandler.java
@@ -2,7 +2,7 @@ package com.bhatn.cashbackking;
 
 import com.amazonaws.serverless.exceptions.ContainerInitializationException;
 import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
-import com.amazonaws.serverless.proxy.model.HttpApiV2ProxyRequest; // <--- V2 is key
+import com.amazonaws.serverless.proxy.model.HttpApiV2ProxyRequest; // <--- This MUST be V2
 import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
@@ -12,11 +12,12 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class StreamLambdaHandler implements RequestStreamHandler {
+    // Note the generic types: <HttpApiV2ProxyRequest, AwsProxyResponse>
     private static final SpringBootLambdaContainerHandler<HttpApiV2ProxyRequest, AwsProxyResponse> handler;
 
     static {
         try {
-            // Using the V2 static helper method
+            // CRITICAL: You must use the 'getHttpApiV2ProxyHandler' method
             handler = SpringBootLambdaContainerHandler.getHttpApiV2ProxyHandler(CashbackKingApplication.class);
         } catch (ContainerInitializationException e) {
             e.printStackTrace();
@@ -27,6 +28,7 @@ public class StreamLambdaHandler implements RequestStreamHandler {
     @Override
     public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context)
             throws IOException {
+        // This method automatically handles the stream conversion for V2
         handler.proxyStream(inputStream, outputStream, context);
     }
 }
