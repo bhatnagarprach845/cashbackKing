20401d151073689e347d735c8be8aece1f018057
Prachi <bhatnagar.prachi845@gmail.com>
2026-03-27 13:17:06 -0400

    Added CORS

--- src/main/java/com/bhatn/cashbackking/StreamLambdaHandler.java
+++ src/main/java/com/bhatn/cashbackking/StreamLambdaHandler.java
@@ -3,6 +3,7 @@ package com.bhatn.cashbackking;
 import com.amazonaws.serverless.exceptions.ContainerInitializationException;
 import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
 import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.model.HttpApiV2ProxyRequest;
 import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
@@ -12,12 +13,14 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class StreamLambdaHandler implements RequestStreamHandler {
-    private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+
+    private static SpringBootLambdaContainerHandler<HttpApiV2ProxyRequest, AwsProxyResponse> handler;
+
     static {
         try {
-            handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(CashbackKingApplication.class);
+            // Change 'getAwsProxyHandler' to 'getHttpApiV2ProxyHandler'
+            handler = SpringBootLambdaContainerHandler.getHttpApiV2ProxyHandler(CashbackKingApplication.class);
         } catch (ContainerInitializationException e) {
-            e.printStackTrace();
             throw new RuntimeException("Could not initialize Spring Boot application", e);
         }
     }
