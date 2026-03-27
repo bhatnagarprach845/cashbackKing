080cdbb399d4f262bed6383d3e93cf088688d1b4
Prachi <bhatnagar.prachi845@gmail.com>
2026-03-27 13:33:54 -0400

    Added CORS

--- src/main/java/com/bhatn/cashbackking/StreamLambdaHandler.java
+++ src/main/java/com/bhatn/cashbackking/StreamLambdaHandler.java
@@ -5,6 +5,7 @@ import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
 import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
 import com.amazonaws.serverless.proxy.model.HttpApiV2ProxyRequest;
 import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+import com.amazonaws.serverless.proxy.spring.SpringBootProxyHandlerBuilder;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 
@@ -13,22 +14,25 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class StreamLambdaHandler implements RequestStreamHandler {
+    private static SpringBootLambdaContainerHandler<HttpApiV2ProxyRequest, AwsProxyResponse> handler;
 
-        private static final SpringBootLambdaContainerHandler<HttpApiV2ProxyRequest, AwsProxyResponse> handler;
-
-        static {
-            try {
-                // This is the most reliable way to get a pre-configured v2 handler
-                handler = SpringBootLambdaContainerHandler.getHttpApiV2ProxyHandler(CashbackKingApplication.class);
-            } catch (ContainerInitializationException e) {
-                e.printStackTrace();
-                throw new RuntimeException("Could not initialize Spring Boot application", e);
-            }
-        }
-
-        @Override
-        public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context)
-                throws IOException {
-            handler.proxyStream(inputStream, outputStream, context);
+    static {
+        try {
+            // Use the Builder to specify HttpApiV2 (Function URL) support
+            handler = new SpringBootProxyHandlerBuilder<HttpApiV2ProxyRequest>()
+                    .defaultProxy()
+                    .asyncInit() // Keeps that 15s timeout away
+                    .springBootApplication(CashbackKingApplication.class)
+                    .buildAndInitialize();
+        } catch (ContainerInitializationException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Could not initialize Spring Boot application", e);
         }
     }
+
+    @Override
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context)
+            throws IOException {
+        handler.proxyStream(inputStream, outputStream, context);
+    }
+}
