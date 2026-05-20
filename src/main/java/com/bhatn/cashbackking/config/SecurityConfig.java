eb673094638920dd28072eab14a067673b541f1b
Prachi <bhatnagar.prachi845@gmail.com>
2026-05-20 16:05:51 -0400

    changed regarding syn

--- src/main/java/com/bhatn/cashbackking/config/SecurityConfig.java
+++ src/main/java/com/bhatn/cashbackking/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/debug/**").permitAll() // ← add this
                         .requestMatchers(org.springframework.http.HttpMethod.OPTIONS, "/**").permitAll()
-                        .requestMatchers("/api/v1/users/**", "/api/v1/payout-status", "/api/v1/receipts/**").authenticated()
+                        .requestMatchers("/api/v1/users/**", "/api/v1/payout-status", "/api/v1/receipts/redeem").authenticated()
                         //.requestMatchers("/api/v1/users/**", "/api/v1/receipts/**").authenticated()
                         .requestMatchers("/api/v1/version").permitAll()
                         .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
