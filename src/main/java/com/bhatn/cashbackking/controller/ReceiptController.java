f292e4b44e5a73f367cccb10ff9fa1b7a5802042
Prachi <bhatnagar.prachi845@gmail.com>
2026-05-20 12:26:37 -0400

    receipt controller

--- src/main/java/com/bhatn/cashbackking/controller/ReceiptController.java
+++ src/main/java/com/bhatn/cashbackking/controller/ReceiptController.java
@@ -28,7 +28,7 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/receipts")
 @Slf4j
 public class ReceiptController {
 
