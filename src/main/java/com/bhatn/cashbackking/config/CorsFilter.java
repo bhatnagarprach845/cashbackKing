1296caf117a03ac21cf4de5eb953be03709b3e64
Prachi <bhatnagar.prachi845@gmail.com>
2026-05-28 11:41:34 -0400

    DuplicateCollision receipt

--- src/main/java/com/bhatn/cashbackking/config/CorsFilter.java
+++ src/main/java/com/bhatn/cashbackking/config/CorsFilter.java
@@ -20,7 +20,7 @@ public class CorsFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
 
-        response.setHeader("Access-Control-Allow-Origin", "https://feature-initialcommit.dwp81oqt95zeu.amplifyapp.com");
+        //response.setHeader("Access-Control-Allow-Origin", "https://feature-initialcommit.dwp81oqt95zeu.amplifyapp.com");
         response.setHeader("Access-Control-Allow-Origin", "https://feature-duplicatecollision.dwp81oqt95zeu.amplifyapp.com");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
